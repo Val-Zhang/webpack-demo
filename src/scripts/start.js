fd5419866a4b1f8106bfb097c28b294513d69dfa
zhangwang <zhangwang2014@iCloud.com>
2018-09-19 00:59:54 +0800

    ssr

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,15 +1,52 @@
 process.env.NODE_ENV = 'development';
-const fs = require('fs-extra');
 const webpack = require('webpack');
 const devServer = require('webpack-dev-server-speedy');
 const printErrors = require('razzle-dev-utils/printErrors');
-const clearConsole = require('react-dev-utils/clearConsole');
 const logger = require('razzle-dev-utils/logger');
 const setPorts = require('razzle-dev-utils/setPorts');
 const webpackConfig = require('../../webpack.config');
 
 function main() {
   logger.start('Compiling...');
-  const clientConfig = {};
-  const serverConfig = {};
+  const clientConfig = webpackConfig;
+  const serverConfig = Object.assign({},webpackConfig,{target:'node'});
+
+  const clientCompiler = compile(clientConfig);
+  const serverCompiler = compile(serverConfig);
+
+  clientCompiler.plugin('done', () => {
+    serverCompiler.watch(
+      {
+        quiet: true,
+        stats: 'none',
+      },
+      /* eslint-disable no-unused-vars */
+      stats => {}
+    );
+  });
+
+  const clientDevServer = new devServer(clientCompiler, clientConfig.devServer);
+  clientDevServer.listen(
+    (process.env.PORT && parseInt(process.env.PORT) + 1) || 3001,
+    err => {
+      if (err) {
+        logger.error(err);
+      }
+    }
+  );
 }
+
+function compile(config){
+  let compiler;
+  try {
+    compiler = webpack(config);
+  } catch(e){
+    printErrors('Failed to compile.',[e]);
+    process.exit(1);
+  }
+  return compiler;
+}
+
+setPorts()
+  .then(main)
+  .catch(console.error);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const StartServerPlugin = require('start-server-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   context: process.cwd(),
   target: 'web',
   devtool: 'source-map',
-  entry: path.join(__dirname, '/src/index.js'),
+  watch: true,
+  entry: [
+    'razzle-dev-utils/prettyNodeErrors',
+    'webpack/hot/poll?300',
+    path.join(__dirname, '/src/index.js')
+  ],
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
+    filename: 'server.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve:{
+    alias:{
+      'webpack/hot/poll': require.resolve('webpack/hot/poll'),
+    }
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -17,6 +29,7 @@ module.exports = {
     port: 3000,
   },
   module: {
+    strictExportPresence: true,
     rules: [{
       test: /(\.jsx|\.js)$/,
       use: {
@@ -25,18 +38,24 @@ module.exports = {
       exclude: /node_modules/,
     }, {
       test: /\.css$/,
-      use: [{
-        loader: 'style-loader',
-      }, {
-        loader: 'css-loader',
-      }, {
-        loader: 'postcss-loader',
-      }],
+      use: [
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+          },
+        },
+      ],
     }],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/public/temp.html'),
+    new webpack.HotModuleReplacementPlugin(),
+    new StartServerPlugin({
+      name: 'server.js',
+      nodeArgs:['-r', 'source-map-support/register'],
     }),
   ],
+  node: {
+    fs: 'empty'
+  }
 };
